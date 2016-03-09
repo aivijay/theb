fe90e7a966a8b6a2347ba412d70b500a4cf7434c
vijay <aivijay@gmail.com>
2016-03-08 18:03:57 -0600

    Adding expecations for testing

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [expectations "2.0.9"]]
+  :plugins [[lein-expectations "0.0.8"]]
   :main ^:skip-aot theb.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
